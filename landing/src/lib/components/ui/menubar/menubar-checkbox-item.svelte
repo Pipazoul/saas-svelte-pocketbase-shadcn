9017cd6f30b54cf050d1a69ac69902eab1713837
Yassin Siouda <yassin@siouda.com>
2024-05-15 18:09:14 +0200

    feat(landing): basic landing page

---
+++ landing/src/lib/components/ui/menubar/menubar-checkbox-item.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Menubar as MenubarPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
+	import Fa from "svelte-fa";
+	import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+	type $$Props = MenubarPrimitive.CheckboxItemProps;
+	type $$Events = MenubarPrimitive.CheckboxItemEvents;
+
+	let className: $$Props["class"] = undefined;
+	export let checked: $$Props["checked"] = false;
+	export { className as class };
+</script>
+
+<MenubarPrimitive.CheckboxItem
+	bind:checked
+	class={cn(
+		"relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50",
+		className
+	)}
+	on:click
+	on:keydown
+	on:focusin
+	on:focusout
+	on:pointerleave
+	on:pointermove
+	on:pointerdown
+	{...$$restProps}
+>
+	<span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+		<MenubarPrimitive.CheckboxIndicator>
+			<Fa icon={faCheck} />
+		</MenubarPrimitive.CheckboxIndicator>
+	</span>
+	<slot />
+</MenubarPrimitive.CheckboxItem>
--- landing/src/routes/+page.svelte
+++ landing/src/routes/+page.svelte
@@ -1,5 +1,100 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-  </script>
-   
-  <Button>Click me</Button>
+	import Logo from '$lib/assets/favicon.svg';
+	import Cover from '$lib/assets/cover.png';
+	import Pattern_1 from '$lib/assets/pattern_1.png';
+	import Pattern_2 from '$lib/assets/pattern_2.png';
+	import Pattern_3 from '$lib/assets/pattern_3.png';
+	import Button from '$lib/components/ui/button/button.svelte';
+  import * as Card from "$lib/components/ui/card";
+  import {env} from '$env/dynamic/public'
+
+  const frontUrl = env.PUBLIC_FRONT_URL;
+  const CurrentYear = new Date().getFullYear();
+
+</script>
+
+<section class="w-screen h-screen">
+  <div class="fixed z-10 flex w-full items-center justify-center">
+    <div
+    class="color-white mt-4 flex w-fit items-center rounded-lg bg-neutral-700 p-2 text-white"
+    >
+      <img src={Logo} alt="logo" class="mr-2 h-8 w-8" />
+      <ul class="flex">
+        <a href="#"><li class="p-1">Item 1</li></a>
+        <a href="#"><li class="p-1">Item 2</li></a>
+        <a href="#"><li class="p-1">Item 3</li></a>
+      </ul>
+      <a href={frontUrl}>
+        <Button variant="secondary">SignIn</Button>
+      </a>
+    </div>
+  </div>
+
+	<div>
+			<div class="w-screen h-96 rounded-b-2xl flex justify-center items-center" style="background-image: url({Cover});">
+        <div>
+          <h1 class="text-7xl">Headline</h1>
+          <p>A catch phrase telling stuff</p>
+          <a href={frontUrl}>
+            <Button >Get Started</Button>
+          </a>
+        </div>
+			</div>
+	</div>
+  <div class="p-4">
+    <h2 class="text-4xl font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo nec ante nec egestas. </h2>
+    <div class="flex">
+      <div class="w-1/2">
+        <Card.Root class="m-4">
+          <Card.Header>
+            <Card.Title>Lorem ipsum</Card.Title>
+            <Card.Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo nec ante nec egestas. </Card.Description>
+          </Card.Header>
+          <Card.Content>
+            <div class="w-full h-96 rounded-2xl flex justify-center items-center" style="background-image: url({Pattern_1});">
+          </Card.Content>
+    
+        </Card.Root>
+      </div>
+      <div class="w-1/2">
+        <Card.Root class="m-4">
+          <Card.Header>
+            <Card.Title>Lorem ipsum</Card.Title>
+            <Card.Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo nec ante nec egestas. </Card.Description>
+          </Card.Header>
+          <Card.Content>
+            <div class="w-full h-96 rounded-2xl flex justify-center items-center" style="background-image: url({Pattern_2});">
+          </Card.Content>
+    
+        </Card.Root>
+      </div>
+    </div>
+    <div class="flex flex-row items-center p-4">
+      <div class="w-1/2 flex justify-center">
+        <div class="w-1/2 space-y-2">
+          <h2 class="text-4xl ">Lorem ipsum dolor sit amet</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo nec ante nec egestas. In laoreet id justo sit amet aliquet
+          </p>  
+          <a href={frontUrl}>
+            <Button >Get Started</Button>
+          </a>    
+        </div>
+
+      </div>
+      <div class="w-1/2 h-96 rounded-2xl flex justify-center items-center bg-cover bg-center" style="background-image: url({Pattern_3});"></div>
+    </div>
+  </div>
+  <div class="h-0.5 bg-neutral-700"></div>
+  <div class="p-4">
+    <div class="flex items-center">
+      <img src={Logo} alt="logo" class="mr-2 h-8 w-8 invert" />
+      <p class="font-bold">SAAS</p>
+    </div>
+    <div class="text-neutral-600 mt-2">
+      © {CurrentYear} All rights reserved.
+    </div>
+  </div>
+  
+  
+</section>
